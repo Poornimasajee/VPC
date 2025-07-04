332d18d0ba17bbff8675efe091e236b6cd979ee9
poornima <poornima02011999@gmail.com>
2025-07-05 00:26:59 +0530

    six commit

--- modules/vpc/main.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source         = "./modules/vpc"
-  vpc_name       = var.vpc_name
-  cidr_block     = var.vpc_cidr
-  public_subnets = var.public_subnets
-  azs            = var.azs
-  
-}
